b3db94e24e2a878379dadd3d6e334742e57818a5
mick <mick@newmediafront.com>
2017-02-28 17:15:44 -0800

    disabling temp sensors for extr 2 and 3

--- src/Configuration.h
+++ src/Configuration.h
@@ -88,8 +88,8 @@
 // 55 is 100k thermistor - ATC Semitec 104GT-2 (Used in ParCan & J-Head) (1k pullup)
 
 #define TEMP_SENSOR_0 1
-#define TEMP_SENSOR_1 1
-#define TEMP_SENSOR_2 1
+#define TEMP_SENSOR_1 0
+#define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_BED 7
 
 // This makes temp sensor 1 a redundant sensor for sensor 0. If the temperatures difference between these sensors is to high the print will be aborted.
